f68411430a5394c47cc2c4b439cf2cb7f615e2d4
A1n2F <alissonfferreira22@gmail.com>
2025-04-04 11:30:41 -0300

    Route Params

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,9 @@ const PORT = 3333
 
 const app = express()
 
+app.get("/products/:id/:user", (request, response) => {
+    const { id, user } = request.params
+    response.end(`Produto ${id} do usuario ${user}`)
+})
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
