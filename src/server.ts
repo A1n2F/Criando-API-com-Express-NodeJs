957ceb4e9df2ee45ae2abaa9cb2c89df45fca402
A1n2F <alissonfferreira22@gmail.com>
2025-04-04 11:46:39 -0300

    Query Params

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,10 @@ const PORT = 3333
 
 const app = express()
 
-app.get("/products/:id/:user", (request, response) => {
-    const { id, user } = request.params
-    response.end(`Produto ${id} do usuario ${user}`)
+app.get("/products", (request, response) => {
+    const {page, limit} = request.query
+
+    response.end(`Page ${page} de ${limit}`)
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
