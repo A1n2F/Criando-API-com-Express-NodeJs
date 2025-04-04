e5746492e4d64efe984dbae162e9b5d0773bb0e0
A1n2F <alissonfferreira22@gmail.com>
2025-04-04 14:48:15 -0300

    Separando as Rotas

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,11 @@
 import express from "express"
-import { myMiddleware } from "./middlewares/my-middleware"
+import { routes } from "./routes"
 
 const PORT = 3333
 
 const app = express()
 app.use(express.json())
 
-//Midlleware Global(Aplica para todas as rotas abaixo)
-// app.use(myMiddleware)
-
-app.get("/products", (request, response) => {
-    const {page, limit} = request.query
-
-    response.send(`Page ${page} de ${limit}`)
-})
-
-//Middleware local em um rota específica
-app.post("/products", myMiddleware, (request, response) => {
-    const {name, price} = request.body
-
-    // response.send(`Produto ${name} custa $ ${price}`)
-
-    response.status(201).json({ name, price, user_id: request.user_id })
-})
+app.use(routes)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
